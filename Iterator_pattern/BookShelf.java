d7c519c3b2cde3afcb0d0e33578c6d598aa789a7
adust09 <shoukitsuda@icloud.com>
2021-06-28 09:54:59 +0900

    indent

--- Iterator_pattern/BookShelf.java
+++ Iterator_pattern/BookShelf.java
@@ -5,6 +5,7 @@ public class BookShelf implements Aggregate{
     public BookShelf(int maxsize){
         this.books=new Book[maxsize];
     }
+
     public Book getBookAt(int index){
         return books[index];
     }
